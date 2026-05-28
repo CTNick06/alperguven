ecde37480bd86300050ef46aec2204d92c4f5ea3
CTNick06 <120687938+CTNick06@users.noreply.github.com>
2026-05-29 00:56:19 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', () => {
       ];
     } else if (document.documentElement.classList.contains('lang-de')) {
       return [
-        "Spezialist für Cybersicherheit"
+        "Spezialist für Cybersicherheit."
       ];
     } else if (document.documentElement.classList.contains('lang-zh')) {
       return [
